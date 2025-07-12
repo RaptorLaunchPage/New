d295c3f7da453e407e2957dfed688edc3346c1a9
RaptorLaunchPage <swarajsxy@gmail.com>
2025-07-12 06:53:34 +0000

    Meta data update

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Raptor Esports Official',
+  description: 'Raptor Esports is a competitive BGMI organisation focused on Performance, growth and excellence in mobile esports',
+  
 }
 
 export default function RootLayout({
